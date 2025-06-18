67dcf8f50e9bf0562c7111d2e099f11ad18e855a
Meghan <afigueroa57@ucol.mx>
2025-06-17 18:54:15 -0600

    Agregando validaciones y experimentación con las validaciones completadas, versión funcinal

--- M5-5_ManejoDeFormularios/script.js
+++ M5-5_ManejoDeFormularios/script.js
@@ -1,3 +1,5 @@
+mensajeError = document.getElementById("errores");
+
 document
   .getElementById("registroEvento")
   .addEventListener("submit", function (event) {
@@ -20,6 +22,80 @@ document
       return;
     }
 
+    // Otras validaciones [3 minimo con mensajes personalizados]
+    if (!validarTelefono(telefono)) {
+      return;
+    }
+    if (!validarCorreo(correo)) {
+      return;
+    }
+    if (!validarFecha(fecha)) {
+      return;
+    }
+
+    mensajeError.textContent = "";
+
     // Si todo está bien
     alert("Registro exitoso. ¡Gracias por registrarte!");
   });
+
+function validarTelefono(telefono) {
+  const regex = /^[0-9]{10}$/;
+  if (regex.test(telefono)) {
+    mensajeError.textContent = "";
+    return true;
+  } else {
+    mensajeError.textContent =
+      "El teléfono debe contener 10 dígitos numéricos.";
+    return false;
+  }
+}
+
+function validarCorreo(correo) {
+  const regex = /^[\w._%+-]+@[\w.-_]+\.[a-zA-Z]{2,8}$/;
+  if (regex.test(correo)) {
+    mensajeError.textContent = "";
+    return true;
+  } else {
+    mensajeError.textContent = "El correo electrónico no es válido.";
+    return false;
+  }
+}
+
+function validarFecha(fecha) {
+  const fechaActual = new Date();
+  const fechaSeleccionada = new Date(fecha);
+
+  if (
+    fechaSeleccionada.getFullYear() < fechaActual.getFullYear() ||
+    fechaSeleccionada.getMonth() < fechaActual.getMonth() ||
+    (fechaSeleccionada.getMonth() === fechaActual.getMonth() &&
+      fechaSeleccionada.getDate() < fechaActual.getDate())
+  ) {
+    mensajeError.textContent =
+      "La fecha del evento no puede ser anterior o la fecha actual.";
+    return false;
+  } else {
+    mensajeError.textContent = "";
+    return true;
+  }
+}
+
+/*
+   *Objetivo
+Crear un formulario funcional que cubra las siguientes características:
+
+1. Capturar datos del usuario como nombre, correo electrónico y teléfono.
+2. Seleccionar intereses usando casillas de verificación.
+3. Elegir el horario preferido con botones de radio.
+4. Seleccionar una fecha para el evento.
+5. Subir un archivo opcional (como una identificación).
+6. Validar los datos antes de enviarlos. 
+*/
+
+/*
+*INSTRUCCIONES
+1. En este código encontrarás un formulario con una gran cantidad de campos y algunas validaciones básicas. Intenta separar los estilos en un archivo .css y el código de JavaScript en un archivo .js
+2. Implementa al menos 3 validaciones adicionales para algunos de los campos, puedes elegir los que desees.
+3. Experimenta con distintos escenarios y asegúrate de que funcione como deseas, incluso puedes crear mensajes personalizados según el campo que estés validando.
+*/
