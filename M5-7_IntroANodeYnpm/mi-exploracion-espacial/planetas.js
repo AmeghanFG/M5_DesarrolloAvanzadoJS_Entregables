fd6cbdadc00f9bc1cff68eb41c550f71533783ca
Meghan <afigueroa57@ucol.mx>
2025-06-26 18:12:31 -0600

    Terminando el paso dos del repositorio https://gist.github.com/heladio-devf-mx/6761648be4a91b72d53b14a48f3fd7c1

---
+++ M5-7_IntroANodeYnpm/mi-exploracion-espacial/planetas.js
@@ -0,0 +1,15 @@
+const planetas = [
+  {
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655",
+  },
+  {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016",
+  },
+  // ¡Añade más planetas!
+];
+
+module.exports = planetas; // Recordatorio: Sirve para exportar el valor de la variable planetas desde un archivo en Node.js, en otro archivo se puede importar con require('./planetas.js') y usar la variable planetas.
